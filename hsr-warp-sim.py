df5cec6e9a40c08f6b23e74d79a9ea17eab27cb8
Brent <107974995+brentelewis@users.noreply.github.com>
2024-07-23 23:08:13 -0500

    Update hsr-warp-sim.py
    -Implemented the pity system!!!
    -Front end is not my strong suit so it looks pretty bad, sorry future employers
    -Can probably clean up the code some

--- hsr-warp-sim.py
+++ hsr-warp-sim.py
@@ -10,6 +10,13 @@ root = Tk()
 # Adjust size 
 root.geometry("500x500")
 
+num_pulls = 0
+pity_5 = 0
+pity_4 = 0
+limited_guarantee = False
+selected_4_guarantee = False
+args = [num_pulls, pity_5, pity_4, limited_guarantee, selected_4_guarantee]
+
 # Dropdown menu options
 # Create a list of 5* limited character options
 five_star_options = []
@@ -65,57 +72,138 @@ def submit():
 	elif four_star_selected1.get() == four_star_selected2.get() or four_star_selected1.get() == four_star_selected3.get() or four_star_selected2.get() == four_star_selected3.get():
 		messagebox.showerror("Error!", "The selected 4* characters cannot be the same.")
 	else:
-		banner_label.config(text = f"YOUR BANNER:"
-			   f"\n{five_star_selected.get()}"
-			   f"\n{four_star_selected1.get()}"
-			   f"\n{four_star_selected2.get()}"
-			   f"\n{four_star_selected3.get()}")
-		warp_button1 = Button(root, text = "WARP x1", command = warp).pack()
-		warp_button1 = Button(root, text = "WARP x10", command = warp_10).pack()
+		warp_button = Button(root, text = "WARP x1", command = lambda: warp(args))
+		warp_button.pack()
+		warp_button10 = Button(root, text = "WARP x10", command = lambda: warp_10(args))
+		warp_button10.pack()
+		banner_label.config(text = f'YOUR BANNER:'
+			   f'\n{five_star_selected.get()}'
+			   f'\n{four_star_selected1.get()}'
+			   f'\n{four_star_selected2.get()}'
+			   f'\n{four_star_selected3.get()}')
 
-def warp():
+def warp(l):
 	base_outcomes = ["5", "4", "3"]
 	base_probabilities = [0.006, 0.051, 0.943]
 	guaranteed_outcomes = ["5", "4"]
 	guaranteed_probabilities = [0.006, .994]
+	
+	updated_four_star_list = standard_options["4"]["Character"]
 
-	result = random.choices(base_outcomes, base_probabilities)[0]
-	if result == "3":
-		result = random.choice(standard_options["3"]["Light Cone"])
-	elif result == "4":
-		if random.choice([0, 1]) == 0:
-			result = random.choice(standard_options["4"]["Character"])
+	if l[0] == 0:
+		updated_four_star_list.remove(four_star_selected1.get())
+		updated_four_star_list.remove(four_star_selected2.get())
+		updated_four_star_list.remove(four_star_selected3.get())
+
+	selected_four_stars_list = [four_star_selected1.get(), four_star_selected2.get(), four_star_selected3.get()]
+
+	if l[1] == 89:
+		if l[3] == True:
+			result = f'!!!!{five_star_selected.get()}!!!!'
+			l[3] = False
 		else:
-			result = random.choice(standard_options["4"]["Light Cone"])
+			roll = random.choice([0, 1])
+			if roll == 0:
+				result = f'!!!!{five_star_selected.get()}!!!!'
+				l[3] = False
+			else:
+				result = f'!!!!{random.choice(standard_options["5"]["Character"])}!!!!'
+				l[3] = True
+		l[1] = 0
+		l[2] = 0
+
+	elif l[2] == 9:
+		result = random.choices(guaranteed_outcomes, guaranteed_probabilities)[0]
+		if result == "4":
+			if l[4] == True:
+				result = random.choice(selected_four_stars_list)
+				l[4] = False
+			else:
+				roll = random.choice([0, 1])
+				if roll == 0:
+					result = random.choice(selected_four_stars_list)
+					l[4] = False
+				else:
+					result = random.choice(updated_four_star_list)
+					l[4] = True
+			l[1] += 1
+			l[2] = 0
+		else:
+			if l[3] == True:
+				result = result = f'!!!!{five_star_selected.get()}!!!!'
+				l[3] = False
+			else:
+				roll = random.choice([0, 1])
+				if roll == 0:
+					result = result = f'!!!!{five_star_selected.get()}!!!!'
+					l[3] = False
+				else:
+					result = f'!!!!{random.choice(standard_options["5"]["Character"])}!!!!'
+					l[3] = True
+			l[1] = 0
+			l[2] = 0
 	else:
-		if random.choice([0, 1]) == 0:
-			result = five_star_selected.get()
+		result = random.choices(base_outcomes, base_probabilities)[0]
+		if result == "3":
+			result = random.choice(standard_options["3"]["Light Cone"])
+			l[1] += 1
+			l[2] += 1
+		elif result == "4":
+			if l[4] == True:
+				result = random.choice(selected_four_stars_list)
+				l[4] = False
+			elif random.choice([0, 1]) == 0:
+				result = random.choice(selected_four_stars_list)
+				l[4] = False
+			else:
+				if random.choice([0, 1]) == 0:
+					result = random.choice(updated_four_star_list)
+				else:
+					result = random.choice(standard_options["4"]["Light Cone"])
+				l[4] = True	
+			l[1] += 1
+			l[2] = 0
 		else:
-			result = random.choice(standard_options["5"]["Character"])
+			if l[3] == True:
+				result = f'!!!!{five_star_selected.get()}!!!!'
+				l[3] = False
+			else:
+				if random.choice([0, 1]) == 0:
+					result = f'!!!!{five_star_selected.get()}!!!!'
+					l[3] = False
+				else:
+					result = f'!!!!{random.choice(standard_options["5"]["Character"])}!!!!'
+					l[3] = True
 
+			l[1] = 0
+			l[2] = 0
+	l[0] += 1
+	
+	print(result, l)
+	max_lines = 10
 	current_text = results_label.cget("text")
 	lines = current_text.splitlines()
 	new_line = result
 	lines.insert(0, new_line)
-	if len(lines) > 10:
+	if len(lines) > max_lines:
 		lines.pop()
 	new_text = "\n".join(lines)
 	results_label.config(text = new_text)
-	
 
-def warp_10():
+def warp_10(l):
 	for _ in range(10):
-		warp()
+		warp(l)
 
 # Create button, it will change label text 
-start_button = Button(root, text = "START", command = submit).pack()
+start_button = Button(root, text = "START", command = submit)
+start_button.pack()
 
 # Create Labels
 banner_label = Label(root, text = " ", anchor = "w", justify = "left")
-banner_label.pack()
+banner_label.pack(fill = X, padx = 10, pady = 10)
 
 results_label = Label(root, text = " ", anchor = "w", justify = "left")
-results_label.pack()
+results_label.pack(fill = X, padx = 10, pady = 10)
 
 # Execute tkinter 
 root.mainloop()
